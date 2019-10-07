4aad4f90b97b96be034d07fad9d69d2fb1716f76
Jobsity <jobsity>
2019-10-07 18:06:37 -0500

    Save example

---
+++ store/src/components/counter.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class Counter extends Component {
+    state = {  }
+    render() { 
+        return ( 
+            <h1>Interesante</h1>
+         );
+    }
+}
+ 
+export default Counter;
